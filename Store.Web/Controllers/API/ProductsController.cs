5f99e1dfc681b2cdb7007fcc43cc6e2ba5a97a55
Rui Rodrigues <rui.coutinho.rodrigues@gmail.com>
2019-03-06 19:46:20 +0000

    Products controller API

---
+++ Store.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Store.Web.Data;
+
+namespace Store.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepositiry)
+        {
+            this.productRepository = productRepositiry;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
